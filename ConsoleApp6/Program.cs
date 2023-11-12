aaa809a89cbe5310fffa37574482e2917ae842b6
maxbtw1 <derzc2020@mail.ru>
2023-11-12 23:48:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,21 @@
+﻿double x0, xn, xnp1, e, m;
+m = 1.3;
+e = 0.001;
+x0 = 2.8;
+xn = x0 - ((x0 * Math.Sin(x0) - 1) / (Math.Sin(x0) + x0 * Math.Cos(x0)));
+Console.WriteLine(xn);
+xnp1 = (Math.Abs(xn * Math.Sin(xn) - 1)) / m;
+Console.WriteLine(xnp1);
+int i = 0;
+while (!(xnp1 < Math.Pow(10, -3)))
+{
+    xn = xn - ((xn * Math.Sin(xn) - 1) / (Math.Sin(xn) + xn * Math.Cos(xn)));
+    xnp1 = (Math.Abs(xn * Math.Sin(xn) - 1)) / m;
+    Console.WriteLine(xn);
+    Console.WriteLine(i);
+    Console.WriteLine();
+    i++;
+}
+Console.WriteLine();
+Console.WriteLine(xn);
+Console.WriteLine(xnp1);
